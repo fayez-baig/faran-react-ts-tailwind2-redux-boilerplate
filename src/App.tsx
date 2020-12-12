66b0c6b498a5372a4cbf7f8b2798b084a06d8281
fayez-baig <coderninja16@gmail.com>
2020-12-12 23:22:36 +0530

    redirect route change

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       {/* Place new routes over this */}
       <Route path="/app" component={Layout} />
       {/* If you have an index page, you can remothis Redirect */}
-      <Redirect exact from="/" to="/login" />
+      <Redirect exact from="/" to="/app/dashboard" />
     </Switch>
   </Router>
 );
